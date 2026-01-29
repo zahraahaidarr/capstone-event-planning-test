d23bdf32edbc5ed79aa9b3758c46706fd19ed10e
zahraa haidar <zahraahaidar510@gmail.com>
2026-01-29 03:40:37 +0200

    edits

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*';
+
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
